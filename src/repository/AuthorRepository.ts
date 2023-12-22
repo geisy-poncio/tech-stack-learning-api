4094c22f140d1ab3737ad0ae548d17968578c40d
Geisy Gabrielli Poncio <geisy.poncio@compasso.com.br>
2023-12-22 15:44:49 -0300

    refactor: add error handling with try-catch in book/author repositories

--- src/repository/AuthorRepository.ts
+++ src/repository/AuthorRepository.ts
@@ -28,108 +28,137 @@ export class AuthorRepository implements AuthorRepositoryInterface {
     }
 
     async getAuthorByName(getAuthorByNameDtoInput: GetAuthorByNameDtoInput) {
-        console.log("AuthorRepository::getAuthorByName::Looking for author");
-        const authorByName = await prisma.author.findFirst({
-            where: {
-              name: getAuthorByNameDtoInput.name,
-              isDeleted: false
-            }
-        });
-        return authorByName;
+        try {
+            console.log("AuthorRepository::getAuthorByName::Looking for author");
+            const authorByName = await prisma.author.findFirst({
+                where: {
+                  name: getAuthorByNameDtoInput.name,
+                  isDeleted: false
+                }
+            });   
+            return authorByName;
+
+        } catch (error) {
+            console.error("AuthorRepository::getAuthorByName::error finding author:", error);
+            throw error;
+        }
     }
 
     async getAllAuthors(getAllAuthorsDtoInput: GetAllAuthorsDtoInput) {
-        console.log("AuthorRepository::getAllAuthors::Looking for authors");
-        const { page, size } = getAllAuthorsDtoInput;
-        
-        const allAuthors = await prisma.author.findMany({ 
-            skip: page * size,
-            take: size,
-            where: { 
-                isDeleted: false 
-            } 
-        });
+        try {
+            console.log("AuthorRepository::getAllAuthors::Looking for authors");
+            const { page, size } = getAllAuthorsDtoInput;
+            
+            const allAuthors = await prisma.author.findMany({ 
+                skip: page * size,
+                take: size,
+                where: { 
+                    isDeleted: false 
+                } 
+            });
+            return allAuthors;
 
-        return allAuthors;
+        } catch (error) {
+            console.error("AuthorRepository::getAllAuthors::error finding authors:", error);
+            throw error;
+        }
      }
  
     async getAuthorById(getAuthorByIdDtoInput: GetAuthorByIdDtoInput) {
-        console.log("AuthorRepository::getAuthorById::Looking for author");
-        const { page, size } = getAuthorByIdDtoInput;
-        
-        const authorById = await prisma.author.findFirst({ 
-            where: { 
-                id: getAuthorByIdDtoInput.id, 
-                isDeleted: false 
-            },
-            include: {
-                books: {
-                    select: {
-                        id: true,
-                        name: true
-                    },
-                    skip: (page ?? 0) * (size ?? 10),
-                    take: size ?? 10,
-                    where: {
-                      isDeleted: false,
+        try {
+            console.log("AuthorRepository::getAuthorById::Looking for author");
+            const { page, size } = getAuthorByIdDtoInput;
+            
+            const authorById = await prisma.author.findFirst({ 
+                where: { 
+                    id: getAuthorByIdDtoInput.id, 
+                    isDeleted: false 
+                },
+                include: {
+                    books: {
+                        select: {
+                            id: true,
+                            name: true
+                        },
+                        skip: (page ?? 0) * (size ?? 10),
+                        take: size ?? 10,
+                        where: {
+                          isDeleted: false,
+                        }
                     }
-                }
-            } 
-        });
-        return authorById;
+                } 
+            });
+            return authorById;
+
+        } catch (error) {
+            console.error("AuthorRepository::getAuthorById::error finding author:", error);
+            throw error;
+        }    
     }
 
     async updateAuthorById(updateAuthorByIdDtoInput: UpdateAuthorByIdDtoInput) {
-        console.log("AuthorRepository::updateAuthorById::Updating author");
-        
-        const { id, name } = updateAuthorByIdDtoInput;
+        try {
+            console.log("AuthorRepository::updateAuthorById::Updating author");
+            
+            const { id, name } = updateAuthorByIdDtoInput;
+    
+            const updateAuthor = await prisma.author.update({
+                where: { id: id },
+                data: { name: name }
+            });
+            return updateAuthor;
 
-        const updateAuthor = await prisma.author.update({
-            where: { id: id },
-            data: { name: name }
-        });
-        return updateAuthor;
+        } catch (error) {
+            console.error("AuthorRepository::updateAuthorById::error updating author:", error);
+            throw error;
+        }
     }
 
     async deleteAuthorById(deleteAuthorByIdDtoInput: DeleteAuthorByIdDtoInput) {
-        console.log("AuthorRepository::deleteAuthorById::Excluding author");
-        const extendedPrisma = prisma.$extends({
-            name: "softDelete",
-            query: {
-                author: {
-                    async delete({model, operation, args, query}){
-                        return await prisma.author.update({
-                            where: {id: args.where.id},
-                            data: {
-                                isDeleted: true,
-                                deletedAt: new Date(),
-                                books: {
-                                    updateMany: {
-                                        where: {authorId: args.where.id},
-                                        data: {
-                                            isDeleted: true,
-                                            deletedAt: new Date()
+        try {
+            console.log("AuthorRepository::deleteAuthorById::Excluding author");
+            const extendedPrisma = prisma.$extends({
+                name: "softDelete",
+                query: {
+                    author: {
+                        async delete({model, operation, args, query}){
+                            return await prisma.author.update({
+                                where: {id: args.where.id},
+                                data: {
+                                    isDeleted: true,
+                                    deletedAt: new Date(),
+                                    books: {
+                                        updateMany: {
+                                            where: {authorId: args.where.id},
+                                            data: {
+                                                isDeleted: true,
+                                                deletedAt: new Date()
+                                            }
                                         }
                                     }
+                                },
+                            })
+                        }
+                    },
+                    book: {
+                        async delete({model, operation, args, query}){
+                            return await prisma.book.update({
+                                where: args.where,
+                                data: {
+                                    isDeleted: true,
+                                    deletedAt: new Date()
                                 }
-                            },
-                        })
-                    }
-                },
-                book: {
-                    async delete({model, operation, args, query}){
-                        return await prisma.book.update({
-                            where: args.where,
-                            data: {
-                                isDeleted: true,
-                                deletedAt: new Date()
-                            }
-                        })
+                            })
+                        }
                     }
                 }
-            }
-        })
+            })
+    
+            return await extendedPrisma.author.delete({ where: {id: deleteAuthorByIdDtoInput.id}});
 
-        return await extendedPrisma.author.delete({ where: {id: deleteAuthorByIdDtoInput.id}});
+        } catch (error) {
+            console.error("AuthorRepository::deleteAuthorById::error when deleting author:", error);
+            throw error;
+        }  
     }
 }
--- src/repository/BookRepository.ts
+++ src/repository/BookRepository.ts
@@ -12,71 +12,101 @@ export const prisma = new PrismaClient();
 
 export class BookRepository implements BookRepositoryInterface{
     async saveBook(createBookDtoInput: CreateBookDtoInput) {
-        console.log("BookRepository::saveBook::Saving book");
-        const newBook = await prisma.book.create({
-            data: {
-                name: createBookDtoInput.name,
-                author: {
-                    connect: { id: createBookDtoInput.authorId }
+        try {
+            console.log("BookRepository::saveBook::Saving book");
+            const newBook = await prisma.book.create({
+                data: {
+                    name: createBookDtoInput.name,
+                    author: {
+                        connect: { id: createBookDtoInput.authorId }
+                    }
                 }
-            }
-        });
-        return newBook;
+            });
+            return newBook;
+
+        } catch (error) {
+            console.error("BookRepository::saveBook::error saving book:", error);
+            throw error;
+        }
     }
 
     async getBookById(getBookByIdDtoInput: GetBookByIdDtoInput) {
-        console.log("BookRepository::getBookById::Looking for book");
-        const findBook = await prisma.book.findFirst({
-            where: {
-                id: getBookByIdDtoInput.id,
-                isDeleted: false
-            },
-            include: {
-                author: true
-            }
-        });
-        return findBook;
+        try {
+            console.log("BookRepository::getBookById::Looking for book");
+            const findBook = await prisma.book.findFirst({
+                where: {
+                    id: getBookByIdDtoInput.id,
+                    isDeleted: false
+                },
+                include: {
+                    author: true
+                }
+            });
+            return findBook;
+
+        } catch (error) {
+            console.error("BookRepository::getBookById::error finding book:", error);
+            throw error;
+        }
     }
 
     async getAllBooks(getAllBooksDtoInput: GetAllBooksDtoInput) {
-        console.log("BookRepository::getAllBook::Looking for books");
-        const { page, size } = getAllBooksDtoInput;
+        try {
+            console.log("BookRepository::getAllBook::Looking for books");
+            const { page, size } = getAllBooksDtoInput;
+    
+            const allBooks = await prisma.book.findMany({ 
+                skip: page * size,
+                take: size,
+                where: { 
+                    isDeleted: false 
+                } 
+            });
+            return allBooks;
 
-        const allBooks = await prisma.book.findMany({ 
-            skip: page * size,
-            take: size,
-            where: { 
-                isDeleted: false 
-            } 
-        });
-        return allBooks;
+        } catch (error) {
+            console.error("BookRepository::getAllBooks::error finding books:", error);
+            throw error;
+        }
     }
 
     async updateBookById(updateBookByIdDtoInput: UpdateBookByIdDtoInput) {
-        console.log("BookRepository::updateBookById::Updating book");
-        const updateBook = await prisma.book.update({
-            where: {
-                id: updateBookByIdDtoInput.id
-            },
-            data: {
-                name: updateBookByIdDtoInput.name,
-                authorId: updateBookByIdDtoInput.authorId
-            }
-        });
-        return updateBook;
+        try {
+            console.log("BookRepository::updateBookById::Updating book");
+            const updateBook = await prisma.book.update({
+                where: {
+                    id: updateBookByIdDtoInput.id
+                },
+                data: {
+                    name: updateBookByIdDtoInput.name,
+                    authorId: updateBookByIdDtoInput.authorId
+                }
+            });
+            return updateBook;
+
+        } catch (error) {
+            console.error("BookRepository::updateBookById::error updating book:", error);
+            throw error;
+        }
     }
 
     async deleteBookById(deleteBookByIdDtoInput: DeleteBookByIdDtoInput) {
-        console.log("BookRepository::deleteBookById::Deleting book");
-        const deleteBook = await prisma.book.update({
-            where: {
-                id: deleteBookByIdDtoInput.id
-            },
-            data: {
-                isDeleted: true,
-                deletedAt: new Date()
-            }
-        });
-        return deleteBook;
+        try {
+            console.log("BookRepository::deleteBookById::Deleting book");
+            const deleteBook = await prisma.book.update({
+                where: {
+                    id: deleteBookByIdDtoInput.id
+                },
+                data: {
+                    isDeleted: true,
+                    deletedAt: new Date()
+                }
+            });
+            return deleteBook;
+
+        } catch (error) {
+            console.error("BookRepository::deleteBookById::error when deleting book:", error);
+            throw error;
+        }
     }
 }
